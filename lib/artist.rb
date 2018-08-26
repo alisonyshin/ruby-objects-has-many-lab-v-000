a5142f4ac9c845842ef102230142d8a9101afd87
Alison Shin <alisonyshin@gmail.com>
2018-08-26 19:42:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,7 +19,7 @@ attr_accessor :name, :songs
   end  
   
   def songs
-    Song.songs.map {|song| song.artist == self}
+    Song.songs.select {|song| song.artist == self}
   end
   
   def self.song_count
