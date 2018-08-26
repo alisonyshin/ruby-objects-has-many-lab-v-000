53c09eca5013ce4f3bf5210d2250200dbf303783
Alison Shin <alisonyshin@gmail.com>
2018-08-26 19:30:53 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+attr_accessor :name
+@@songs = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def songs
+    @songs
+  end
+ 
+  def add_song(existing_song)
+    existing_song.artist = self
+    @songs << existing_song
+    @@songs << @songs
+  end  
+  
+  def add_song_by_name(new_song)
+    song = Song.new(new_song)
+    song.artist = self
+    @songs << song
+    @@songs << @songs
+  end  
+  
+  def self.song_count
+    @@songs.flatten.length
+  end
+  
+end
